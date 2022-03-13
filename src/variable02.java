15909d648ddd69bacef50ca218c55a5187c91427
RinaAraki <arakipomu1206@gmail.com>
2022-03-13 13:33:44 +0900

    [java]定数の学習

---
+++ src/variable02.java
@@ -0,0 +1,11 @@
+
+public class variable02 {
+	public static void main(String[] args) {
+		final int MIN_NUMBER = 10;
+//		変数の宣言
+		System.out.println(MIN_NUMBER);
+
+//		再代入しようとするとエラーが出る
+//		MIN_NUMBER = 20; ←エラー
+	}
+}
