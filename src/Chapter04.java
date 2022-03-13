63bfd494f193965f32e26c4a28c90b2b7b06b429
RinaAraki <arakipomu1206@gmail.com>
2022-03-13 13:42:16 +0900

    [java]３章の確認問題

---
+++ src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "pomu";
+		System.out.println(name);
+		
+		name = "Hello";
+		System.out.println(name);
+		
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
--- src/Variable01.java
+++ src/Variable01.java
@@ -2,16 +2,23 @@
 public class Variable01 {
 	public static void main(String[] args) {
 		int num1;
+//		int型の変数num1を宣言する
 		String str1;
+//		string型の変数str1を宣言する
 		num1 = 10;
+//		num1を初期化(10を代入)
 		str1 = "Hello World!";
+//		str1を初期化(Hello Worldを代入)
 		System.out.println(num1);
 		System.out.println(str1);
+//		num1、str1を出力
 		
 		num1 = 20;
+//		num1に20を再代入
 		System.out.println(num1);
 		
 		int num2 = 30;
+//		int型の変数num2を宣言と同時に初期化(30を代入)
 		System.out.println(num2);
 	}
 }
