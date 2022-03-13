96b867b42222ae923eff0abe2019c85e8ce992ea
RinaAraki <arakipomu1206@gmail.com>
2022-03-13 16:03:13 +0900

    [java]５章の確認問題

---
+++ src/Chapter05.java
@@ -0,0 +1,20 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+
+	long long1 =  9223372036854775806L;
+	System.out.println(long1);
+	double double1 = 4.5;
+	System.out.println(double1);
+	boolean bool1 = true;
+	System.out.println(bool1);
+	Float float1 = 0.125f;
+	System.out.println(float1);
+	Integer int1 = 2000000000;
+	System.out.println(int1);
+	Character char1 = 0x3042;
+	System.out.println(char1);
+	int[] numbers = {1,2,3,4,5};
+	System.out.println(numbers[3]);
+	}
+}
--- src/Null01.java
+++ src/Null01.java
@@ -5,5 +5,13 @@ public class Null01 {
 		string1 = "あいう";
 		System.out.println(string1 + "の文字数：" + string1.length());
 //		length=長さ
+		
+		string1 = "";
+		System.out.println(string1);
+//		空文字を出力
+		
+		string1 = null;
+		System.out.println(string1 + "の文字数：" + string1.length() );
+//		nullの変数を参照するとNullpointerExceptionのエラー
 	}
 }
