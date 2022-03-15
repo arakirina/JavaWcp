7c7a5550c63eb73348ff4de7f10d95fd1cb6635d
RinaAraki <arakipomu1206@gmail.com>
2022-03-15 11:22:19 +0900

    [Java]７章の確認問題

--- src/Chapter06.java
+++ src/Chapter06.java
@@ -11,7 +11,24 @@ public class Chapter06 {
 		System.out.println(bool);
 		bool = val < 50 && val % 10 == 0;
 		System.out.println(bool);
+//		
+//
+//		解答
 		
+//		int val = 2 + 5;
+//        System.out.println(val);
+//
+//        val++;
+//        System.out.println(val);
+//
+//        val *= 5;
+//        System.out.println(val);
+//
+//        boolean bool = (val == 50);
+//        System.out.println(bool);
+//
+//        bool = (val < 50) && (val % 10 == 0);
+//        System.out.println(bool);
 	}
-
 }
+
