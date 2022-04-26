10323c3aacb5dbb130180fdb239b01fac2e7824e
RinaAraki <arakipomu1206@gmail.com>
2022-04-26 12:02:18 +0900

    [java]

--- src/classMethod/human/Human05.java
+++ src/classMethod/human/Human05.java
@@ -1,5 +1,24 @@
 package classMethod.human;
 
-public class Human05 {
+//アクセス修飾子
 
+public class Human05 {
+	private String name;
+//	直接参照ができないようにprivateにしている
+	private int age;
+	
+	public Human05() {
+		name = "山田";
+		age = 20;
+	}
+	
+	public String getName() {
+		return name;
+//		getをつけることで、privateがついた変数をgetつければ参照できる
+//		getNameいう名前を使えば、nameを返してくれる、というメソッド。
+	}
+	
+	public int getAge() {
+		return age;
+	}
 }
