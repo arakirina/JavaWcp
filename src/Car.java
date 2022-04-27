f640c75b279eae02f84602ce9fcda359462fb534
RinaAraki <arakipomu1206@gmail.com>
2022-04-27 12:14:01 +0900

    [java]

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,21 @@
 
 public class Car {
-//	private double fuelCost;
-//	private double fuelAmount;
-//	
-//	Car car = new Car
+	
+	private double fuelCost;
+	private double fuelAmount;
+	
+	public Car(double fuelCost, double fuelAmount) {
+		this.fuelCost = fuelCost;
+		this.fuelAmount = fuelAmount;
+	}
+	public void move(int km) {
+		System.out.println(km+"km走ります");
+		this.fuelAmount -= (km / fuelCost);
+//		引き続ける
+	}
+	
+	public double getFuelAmount() {
+		return this.fuelAmount;
+	}
+	
 }
